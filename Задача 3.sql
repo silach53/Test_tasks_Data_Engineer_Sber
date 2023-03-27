743b00661564544b89d513c312c68f46a39ee16b
silach53 <48329361+silach53@users.noreply.github.com>
2023-03-28 00:34:05 +0300

    Update Задача 3.sql

--- Задача 3.sql
+++ Задача 3.sql
@@ -11,12 +11,11 @@ INSERT INTO item_prices (item_id, item_name, item_price, created_dttm) VALUES
     (2, 'Product B', 15.00, '2022-01-15'),
     (2, 'Product B', 16.00, '2022-02-15'),
     (2, 'Product B', 18.00, '2022-03-01');
-    
+
 SELECT
     ip.item_id,
     ip.item_name,
     ip.item_price,
-    ip.created_dttm,
     ip.created_dttm AS valid_from_dt,
     COALESCE((LEAD(ip.created_dttm) OVER (PARTITION BY ip.item_id ORDER BY ip.created_dttm))::date - INTERVAL '1 DAY', '9999-12-31') AS valid_to_dt
 FROM
